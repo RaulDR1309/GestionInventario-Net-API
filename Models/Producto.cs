c9d6e5e7d0594e7c79e3926b4106e5989d53a7ab
Octavio Díaz <rdiaz0908@gmail.com>
2026-02-17 19:27:50 -0600

    Agregar archivos de proyecto.

---
+++ Models/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations; // Para usar validaciones
+
+namespace GestionInventario.Api.Models
+{
+    public class Producto
+    {
+        [Key] // Indica que este será el ID único en la base de datos
+        public int Id { get; set; }
+
+        [Required] // Indica que el nombre no puede estar vacío
+        public string Nombre { get; set; } = string.Empty;
+
+        public string? Descripcion { get; set; } // El "?" significa que puede ser nulo
+
+        public decimal Precio { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
